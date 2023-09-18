11e4121965523821f4c4fd267828ace327a50a85
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-09-18 09:57:55 +0900

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,6 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cart_items, dependent: :destroy
-  has_many :orders, dpendent: :destroy
+  has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
 end
