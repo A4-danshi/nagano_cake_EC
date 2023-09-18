ba657691e81422ea944d50de257084ab82700f6a
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-09-18 00:43:26 +0000

    担当範囲のmigrateと全体的にasociate追記

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
 end
--- db/migrate/20230916072045_create_items.rb
+++ db/migrate/20230916072045_create_items.rb
@@ -1,7 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-
+      t.integer :genre_id
+      t.string :name
+      t.text :introduction
+      t.integer :price
+      t.boolean :is_sale, default: true
       t.timestamps
     end
   end
