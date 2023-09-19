0db4d1f042e2483cbca926f6d729f876f409641d
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-09-19 09:09:26 +0000

    注文情報確認画面、サンクスページ

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,8 +3,32 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-
+  
   def confirm
+    @cart_items = current_customer.cart_item
+    @order = Order.new(order_params)
+    @order.postage = 800
+    @order.payment_method = params[:order][:payment_method]
+    if params[:order][:select_address] == 0
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = cuurent_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address] == 1
+      @address = Address.find(params[:order][:address_id])
+      @order.post_code = @address.post_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else params[:order][:select_address] == 2
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
+  end  
+  
+  def create
+    
+    
+    
   end
 
   def complete
@@ -15,4 +39,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :post_code, :address, :name)
+  end
+
 end
