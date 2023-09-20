5586d140511b53d4d215e9a9f3056437411d3efe
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-09-20 04:07:51 +0000

    注文情報入力・確認・サンクス画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,32 +3,35 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def confirm
-    @cart_items = current_customer.cart_item
+    @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
     @order.postage = 800
     @order.payment_method = params[:order][:payment_method]
-    if params[:order][:select_address] == 0
+
+    if params[:order][:select_address] == "0"
       @order.post_code = current_customer.post_code
       @order.address = current_customer.address
-      @order.name = cuurent_customer.last_name + current_customer.first_name
-    elsif params[:order][:select_address] == 1
-      @address = Address.find(params[:order][:address_id])
-      @order.post_code = @address.post_code
-      @order.address = @address.address
-      @order.name = @address.name
-    else params[:order][:select_address] == 2
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address] == "1"
+       @address = Address.find(params[:order][:address_id])
+       @order.post_code = @address.post_code
+       @order.address = @address.address
+       @order.name = @address.name
+    elsif params[:order][:select_address] == "2"
       @order.post_code = params[:order][:post_code]
       @order.address = params[:order][:address]
       @order.name = params[:order][:name]
+    else
+      flash[:notice] = "宛先を選択してください。"
     end
-  end  
-  
+
+  end
+
   def create
-    
-    
-    
+    @order = Order.new(order_params)
+
   end
 
   def complete
@@ -43,7 +46,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :post_code, :address, :name)
+    params.require(:order).permit(:payment_method, :post_code, :address, :name, :total_payment, :status)
   end
 
 end
