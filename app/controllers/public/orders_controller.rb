049e022f0674ec6ac09bb1ca6448272bf33a4f82
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-09-20 07:42:28 +0000

    注文履歴画面、注文履歴詳細画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -28,24 +28,38 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    
-    
-
+    @cart_items = current_customer.cart_items.all
+    @order = Order.new(order_params)
+    # @order.customer = @customer
+    @order.save
+        # @order.update(status: "waiting")        # ステータスを「入金待ち」に更新
+    @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.purchase_price = cart_item.item.with_tax_price
+      @order_detail.amount = cart_item.amount
+      @order_detail.order_id = order.id
+      @order_detail.item_id = cart_item.item.id
+      @order_detail.save
+    end
+    current_customer.cart_items.destroy_all # カート内の商品を削除
+    redirect_to public_order_complete
   end
 
   def complete
   end
 
   def index
+    @oders = current_customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :post_code, :address, :name, :total_payment, :status)
+    params.require(:order).permit(:payment_method, :post_code, :address, :name, :total_payment, :status, :customer_id)
   end
 
 end
