61e407092c346b6d780369e7f3a26d5094c2fdf0
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-09-20 08:07:36 +0000

    ノータッチ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_19_035828) do
+ActiveRecord::Schema.define(version: 2023_09_20_022940) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(version: 2023_09_19_035828) do
   create_table "addresses", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id"
+    t.string "post_code"
+    t.string "address"
+    t.string "name"
   end
 
   create_table "admins", force: :cascade do |t|
