920bc81fd48757ac98fbea51149de2456db27fbf
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-09-20 08:48:06 +0000

    注文履歴詳細画面　要修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_to request.referer
+  end
+
+  private
+
+  def order_params
+    params.require(:order_detail).permit(:production_status)
+  end
 end
