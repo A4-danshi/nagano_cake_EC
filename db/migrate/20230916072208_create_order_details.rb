e452ed2f340cd189ecd26db82b2c7ffd5e382f79
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-09-21 08:23:08 +0000

    修正

--- db/migrate/20230916072208_create_order_details.rb
+++ db/migrate/20230916072208_create_order_details.rb
@@ -1,12 +1,6 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
-      t.integer :order_id
-      t.integer :item_id
-      t.integer :purchase_price
-      t.integer :amount
-      t.integer :status
-
       t.timestamps
     end
   end
