a12fa0629ffd70612b0d70f49791915e09aa73f7
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-09-21 06:03:10 +0000

    レイアウト調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,10 @@
 class Public::HomesController < ApplicationController
+  def top
+    @items = Item.order(created_at: :DESC)
+    @genres = Genre.all
+    @count = 0
+  end
+
+  def about
+  end
 end
