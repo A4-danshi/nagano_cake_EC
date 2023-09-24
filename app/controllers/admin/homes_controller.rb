0e1230df09c094e5a955fa88765963daddadf4fa
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-09-24 02:58:10 +0000

    注文ステータス・製作ステータスの関連更新機能

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
-    @orders = Order.all
+    @orders = Order.all.order(created_at: :desc)
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,7 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    update_production_status
     redirect_to request.referer
   end
 
@@ -13,5 +14,11 @@ class Admin::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:status)
+  end
+
+  def update_production_status
+    if order_params[:status] == "confirm_payment"
+      @order.order_details.update_all(production_status: 1)
+    end
   end
 end
